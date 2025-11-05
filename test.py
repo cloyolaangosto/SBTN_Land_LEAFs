eb16b90b227ffe1ed582266fa4c5f185acc0712e
cloyolaangosto <cloyolaangosto@gmail.com>
2025-11-05 16:25:31 -0500

    Use path helpers in test script

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ print("Hello world")
 
 
 import sbtn_leaf.map_plotting as mp
+from sbtn_leaf.paths import data_path, project_path
 import geopandas as gpd
 
-raster_test = "LEAFs/soil_erosion/raster/se_rate_25km_Broadleaf_Deciduous_Boreal_dry.tif"
-world_global_hr = gpd.read_file("data/world_maps/high_res/ne_10m_admin_0_countries.shp")
+raster_test = project_path(
+    "LEAFs", "soil_erosion", "raster", "se_rate_25km_Broadleaf_Deciduous_Boreal_dry.tif"
+)
+world_global_hr = gpd.read_file(
+    data_path("world_maps", "high_res", "ne_10m_admin_0_countries.shp")
+)
 
 mp.plot_raster_histogram(raster_test)
